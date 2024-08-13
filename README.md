5131c241d8d691454e6174834ee6d1863ea94649
incotelanna23 <twispro.ananya@gmail.com>
2024-08-13 20:07:02 +0530

    final-commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Quiz App
+
+This Simple Quiz App in React.js allows users to test their knowledge on various topics. The app displays a series of questions, tracks user responses, and provides immediate feedback on their performance. Users can navigate through the quiz, and at the end, view their results and retry the quiz.
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./App.css";
+import questions from "./constants/questions.json";
+import Questions from "./components/Questions";
+import Result from "./components/Result";
+const App = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const handleNextQuestion = (isCorrect) => {
+    setCurrentQuestion(currentQuestion + 1);
+    setUserAnswers([...userAnswers, isCorrect]);
+  };
+  const resetQuiz = () => {
+    setCurrentQuestion(0);
+    setUserAnswers([]);
+  };
+  return (
+    <div className="App">
+      <h1>World Quiz</h1>
+      {currentQuestion < questions.length && (
+        <Questions
+          question={questions[currentQuestion]}
+          onAnswerClick={handleNextQuestion}
+        />
+      )}
+      {currentQuestion === questions.length && (
+        <Result
+          userAnswers={userAnswers}
+          questions={questions}
+          resetQuiz={resetQuiz}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
